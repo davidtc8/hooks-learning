8e2d4d6fd51a4a8900ccfd99970aed925a5563de
dtorresc8 <124192927+dtorresc8@users.noreply.github.com>
2023-06-05 09:47:25 -0600

    Main JSX

--- hook-app/src/main.jsx
+++ hook-app/src/main.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'
 import './index.css';
-import { Padre } from './07-tarea-memo/Padre';
+import { MainApp } from './09-useContext/MainApp';
 
 
+// import { TodoApp } from './08-useReducer/TodoApp';
+// import { Padre } from './07-tarea-memo/Padre';
+// import './08-useReducer/intro-reducer';
 // import { CallbackHook } from './06-memos/CallbackHook';
 // import { MemoHook } from './06-memos/MemorizeHook';
 // import { Memorize } from './06-memos/Memorize';
@@ -17,7 +21,9 @@ import { Padre } from './07-tarea-memo/Padre';
 // import { HooksApp } from './HooksApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  //<React.StrictMode>
-    <Padre />
-  //</React.StrictMode>,
-)
+  <BrowserRouter>
+    {/* <React.StrictMode> */}
+      <MainApp />
+    {/* </React.StrictMode> */}
+  </BrowserRouter>
+  )
